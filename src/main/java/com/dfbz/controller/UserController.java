7bfb7d9ae20e2d0c55c73a35854d98673bac1146
xiaohui <xiaohui@admin.com>
2023-02-16 17:59:42 +0800

    初始化项目

---
+++ src/main/java/com/dfbz/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.dfbz.controller;
+
+/**
+ * @author lscl
+ * @version 1.0
+ * @intro:
+ */
+public class UserController {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+        System.out.println("bbb");
+    }
+}
